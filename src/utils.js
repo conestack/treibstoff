fcbcc7abc33cebfc0eaa31d7bfd66ab2d4e098da
Robert Niederreiter <office@squarewave.at>
2021-08-12 11:51:05 +0200

    Add cookie helpers to utils

--- src/utils.js
+++ src/utils.js
@@ -138,6 +138,36 @@ export function parse_path(url, include_query) {
     return path;
 }
 
+export function create_cookie(name, value, days) {
+    var date,
+        expires;
+    if (days) {
+        date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = name + "=" + escape(value) + expires + "; path=/;";
+}
+
+export function read_cookie(name) {
+    var nameEQ = name + "=",
+        ca = document.cookie.split(';'),
+        i,
+        c;
+    for(i = 0; i < ca.length;i = i + 1) {
+        c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return unescape(c.substring(nameEQ.length, c.length));
+        }
+    }
+    return null;
+}
+
 export const svg_ns = 'http://www.w3.org/2000/svg';
 
 export function set_svg_attrs(el, opts) {
