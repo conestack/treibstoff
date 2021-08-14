93fa7d832b0c3a73773db066db0e848475d49256
Robert Niederreiter <office@squarewave.at>
2021-08-14 07:42:08 +0200

    clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,13 +4,13 @@
 
 set -e
 
-for file in lib64 package-json.lock pyvenv.cfg treibstoff-*; do
+for file in lib64 package-lock.json pyvenv.cfg treibstoff-*; do
     if [ -e "$file" ]; then
         rm "$file"
     fi
 done
 
-for dir in bin build bundle dist include karma lib node_modules share; do
+for dir in bin build bundle dist docs/html include karma lib node_modules share treibstoff.egg-info; do
     if [ -d "$dir" ]; then
         rm -r "$dir"
     fi
