11217906986e379a756f7e566da4c79dfde5d423
Robert Niederreiter <rnix@squarewave.at>
2022-05-18 12:07:46 +0200

    Test FormSelect

--- tests/test_form.js
+++ tests/test_form.js
@@ -1,18 +1,19 @@
 import $ from 'jquery';
 import {
     FormInput,
+    FormSelect,
     lookup_form_elem
 } from '../src/form.js';
 
 QUnit.module('treibstoff.form', hooks => {
     let container;
 
-    hooks.before(assert => {
+    hooks.beforeEach(assert => {
         container = $('<div></div>');
         $('body').append(container);
     });
 
-    hooks.after(() => {
+    hooks.afterEach(() => {
         container.remove();
     });
 
@@ -74,5 +75,69 @@ QUnit.module('treibstoff.form', hooks => {
         assert.ok(input.disabled);
         input.disabled = false;
         assert.false(input.disabled);
+    });
+
+    QUnit.test('Test FormSelect', assert => {
+        let elem = $(`
+            <select>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+            </select>
+        `);
+        let selection = new FormSelect({elem: elem});
+        let options = [];
+        for (let opt of selection.options) {
+            options.push([opt.value, opt.text]);
+        }
+        assert.deepEqual(options, [['1', 'Option 1'], ['2', 'Option 2']]);
+
+        selection.options = [['3', 'Option 3'], ['4', 'Option 4']];
+        options = [];
+        for (let opt of selection.options) {
+            options.push([opt.value, opt.text]);
+        }
+        assert.deepEqual(options, [['3', 'Option 3'], ['4', 'Option 4']]);
+
+        selection.options = [
+            new Option('Option 1', '1'),
+            new Option('Option 2', '2')
+        ];
+        options = [];
+        for (let opt of selection.options) {
+            options.push([opt.value, opt.text]);
+        }
+        assert.deepEqual(options, [['1', 'Option 1'], ['2', 'Option 2']]);
+
+        assert.deepEqual(selection.value, '1');
+        selection.options[1].selected = true;
+        assert.deepEqual(selection.value, '2');
+
+        selection.on('on_change', function(evt) {
+            assert.step(this.value);
+        }.bind(selection));
+        selection.elem.trigger('change');
+        assert.verifySteps(['2']);
+
+        container.append(`
+            <select id="input-formname-fieldname">
+                <option value="a" selected="selected">A</option>
+            </select>
+        `);
+        selection = new FormSelect({
+            form: {name: 'formname'},
+            name: 'fieldname'
+        });
+        assert.deepEqual(selection.elem.attr('id'), 'input-formname-fieldname');
+        assert.deepEqual(selection.form, {name: 'formname'});
+        assert.deepEqual(selection.name, 'fieldname');
+
+        selection.on('on_change', function(evt) {
+            assert.step(this.value);
+        }.bind(selection));
+        selection.elem.trigger('change');
+        assert.verifySteps(['a']);
+
+        selection.clear();
+        assert.deepEqual(selection.options.length, 0);
     });
 });
