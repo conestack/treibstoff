de3d89332497890411c8279eaf86dd87b3ce41e3
Robert Niederreiter <office@squarewave.at>
2022-05-31 14:50:08 +0200

    Remove optional pyramid static view. Fix resource directory.

--- treibstoff/__init__.py
+++ treibstoff/__init__.py
@@ -1,9 +1,11 @@
+import os
 import webresource as wr
 
 
+resources_dir = os.path.join(os.path.dirname(__file__), 'bundle')
 resources = wr.ResourceGroup(
     name='treibstoff',
-    directory='./bundle',
+    directory=resources_dir,
     path='treibstoff'
 )
 resources.add(wr.ScriptResource(
@@ -17,17 +19,3 @@ resources.add(wr.StyleResource(
     depends='bootstrap-css',
     resource='treibstoff.css'
 ))
-
-
-###############################################################################
-# pyramid static view creation if installed
-###############################################################################
-
-try:
-    from pyramid.static import static_view
-    PYRAMID_INSTALLED = True
-except ImportError:
-    PYRAMID_INSTALLED = False
-
-if PYRAMID_INSTALLED:
-    static = static_view('bundle', use_subpath=True)
