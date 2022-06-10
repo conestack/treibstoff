4dd68fb0c355646f6b720321203b562d56e4f426
Robert Niederreiter <rnix@squarewave.at>
2022-06-10 13:52:16 +0200

    Re-add conditional pyramid static view until all cone related packages are migrated to webresource.

--- treibstoff/__init__.py
+++ treibstoff/__init__.py
@@ -19,3 +19,17 @@ resources.add(wr.StyleResource(
     depends='bootstrap-css',
     resource='treibstoff.css'
 ))
+
+
+###############################################################################
+# pyramid static view creation if installed
+###############################################################################
+
+try:
+    from pyramid.static import static_view
+    PYRAMID_INSTALLED = True
+except ImportError:
+    PYRAMID_INSTALLED = False
+
+if PYRAMID_INSTALLED:
+    static = static_view('bundle', use_subpath=True)
